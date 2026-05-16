3163043d0992a3aca5bc827d8fb18f623319013f
lucca <953627561@qq.com>
2026-05-16 17:21:03 +0800

    fix: add turbopack:{} to allow build

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["mysql2"],
-  },
-  // 强制 mysql2 不进入客户端打包
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
+  // 允许 turbopack 构建
+  turbopack: {},
 };
 
 export default nextConfig;
